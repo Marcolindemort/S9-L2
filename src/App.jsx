8c119a3a08be8e270c636dc1937c0c7613be61a5
Marcolindemort <marco.mandolini15@gmail.com>
2023-12-19 16:49:02 +0100

    nav footer card added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
+import Alert from "./components/Alert";
 
 function App() {
 	return (
 		<div className="App">
 			<MyNav />
+			<Alert />
+
+			<MyFooter />
 		</div>
 	);
 }
---
+++ src/components/Alert.jsx
@@ -0,0 +1,9 @@
+const Alert = () => (
+	<Alert variant="info">
+		<Alert.Heading>EPIBOOKS!</Alert.Heading>
+		<hr />
+		<p>Guarda che belle copertine!</p>
+	</Alert>
+);
+
+export default Alert;
